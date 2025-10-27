629f384ea4169feff39da8f0054b90d143b7b936
Zaio Klepoyshkov <lisi4ok@gmail.com>
2025-10-27 20:16:17 +0200

    Add Api Endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ContactController;
+use App\Http\Controllers\Api\InteractionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +11,5 @@ Route::middleware(['auth:sanctum'])->group(function () {
     });
 
     Route::apiResource('contact', ContactController::class);
+    Route::apiResource('interaction', InteractionController::class);
 });
